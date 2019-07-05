e5998b3cc190f8aced22f653027120106d8706ec
Luie <luiejohn.24@gmail.com>
2019-07-05 17:36:53 +0800

    added more section on homepage

--- src/Layout/Public/home/home.js
+++ src/Layout/Public/home/home.js
@@ -1,6 +1,10 @@
 import React, { Fragment } from 'react';
 
 import './home.scss';
+import gallery01 from '../../../assets/img/nat-1.jpg';
+import gallery02 from '../../../assets/img/nat-2.jpg';
+import gallery03 from '../../../assets/img/nat-3.jpg';
+
 
 const Home = () => {
 
@@ -13,12 +17,49 @@ const Home = () => {
                 </div>
 
                 <div className="home-main__section1">
-                    <div className="home-main__item">
-                        
+                    <div className="home-main__section-cont">
+                            <div className="home-main__item-text">
+                                <div className="heading-small">
+                                    You're going to fall in love with nature.
+                                </div>
+
+                                <p className="paragraph-text">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam earum nulla ea enim alias non incidunt? Explicabo aspernatur, enim tempore inventore ea itaque quasi fugit id et in sequi blanditiis?
+                                </p>
+
+                                <div className="heading-small">
+                                    Live adventures like you have never seen before.
+                                </div>
+
+                                <p className="paragraph-text">
+                                    Quam earum nulla ea enim alias non incidunt? Explicabo aspernatur, enim tempore inventore ea itaque quasi fugit id et in sequi blanditiis?
+                                </p>
+                            </div>
+
+                            <div className="home-main__item-images">
+                                <img src={gallery01} alt="" className="home-main__photos home-main__photos-01"/>
+                                <img src={gallery02} alt="" className="home-main__photos home-main__photos-02"/>
+                                <img src={gallery03} alt="" className="home-main__photos home-main__photos-03"/>
+                            </div>
                     </div>
 
-                    <div className="home-main__item">
+                </div>
 
+                <div className="section2">
+                    <div className="section2__card">
+                        asdsa
+                    </div>
+                    
+                    <div className="section2__card">
+                        asdasd
+                    </div>
+
+                    <div className="section2__card">
+                        asdsad
+                    </div>
+
+                    <div className="section2__card">
+                        asdasd
                     </div>
                 </div>
 
