2c1b0215cf16f90c156d82d5bf1c7e520f8dba4e
Luie <luiejohn.24@gmail.com>
2019-07-08 07:59:20 +0800

    media queries start

--- src/Layout/Public/home/home.js
+++ src/Layout/Public/home/home.js
@@ -149,10 +149,53 @@ const Home = ({isLogin, handleChange}) => {
 
                 </section>
 
-                <section className="section_cont3">
+                <section className="section-cont3">
+                    <section className="section-book">
+                        <div className="book">
+                            <div className="book__form">
+                                <form action="#" className="form">
+                                    <div className="u-center-text u-margin-bottom-md">
+                                            <h2 className="heading-small mb-2">
+                                                Start Booking now
+                                            </h2>
+                                    </div>
+                                    <div className="form__group">
+                                        <input type="text" className="form__input" id="name" placeholder="Full Name" required/>
+                                        <label htmlFor="name" className="form__label">Full Name</label>
+                                    </div>
+                                    <div className="form__group">
+                                        <input type="email" className="form__input" id="email" placeholder="Email Address" required/>
+                                        <label htmlFor="email" className="form__label">Email Address</label>
+                                    </div>
 
+                                    <div className="form_group u-margin-bottom-md">
+                                        <div className="form__radio-group">
+                                            <input type="radio" className="form__radio-input" id="small" name="size"/>
+                                            <label htmlFor="small" className="form__radio-label">
+                                                <span className="form__radio-button"></span>
+                                                Small tour group
+                                            </label>
+                                        </div>
+
+                                        <div className="form__radio-group">
+                                                <input type="radio" className="form__radio-input" id="large" name="size"/>
+                                                <label htmlFor="large" className="form__radio-label">
+                                                    <span className="form__radio-button"></span>
+                                                    Large tour group
+                                                </label>
+                                        </div>
+                                    </div>
+
+                                    <div className="submit-form-group mt-4">
+                                            <button className="btn btn-subscribe">Next Step &rarr;</button>
+                                    </div>
+
+                                </form>
+                            </div>
+                        </div>
+
+                    </section>
                 </section>
-
             </div>
         </Fragment>
     )
