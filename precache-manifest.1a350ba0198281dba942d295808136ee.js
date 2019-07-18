168b5ee7d98ab264eda0e08f11074b1624053e93
Luie <luiejohn.24@gmail.com>
2019-07-18 14:08:01 +0800

    Updates

---
+++ precache-manifest.1a350ba0198281dba942d295808136ee.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "830f1d7a7c65da2a05cb3477ae3cbee4",
+    "url": "/FootPrint/index.html"
+  },
+  {
+    "revision": "a843d7f063b682f4e157",
+    "url": "/FootPrint/static/css/main.3553984b.chunk.css"
+  },
+  {
+    "revision": "9b836ba596af483a1429",
+    "url": "/FootPrint/static/js/2.e64163ae.chunk.js"
+  },
+  {
+    "revision": "a843d7f063b682f4e157",
+    "url": "/FootPrint/static/js/main.29a2b79e.chunk.js"
+  },
+  {
+    "revision": "1c991987aa68943976b8",
+    "url": "/FootPrint/static/js/runtime~main.9092e12a.js"
+  },
+  {
+    "revision": "0670a98b8aa1a7eb0a0c37f3cdfba06b",
+    "url": "/FootPrint/static/media/chevron-thin-right.0670a98b.svg"
+  },
+  {
+    "revision": "f9ddb2055ff88bacc9a5f4634c72a30e",
+    "url": "/FootPrint/static/media/enchant.f9ddb205.jpg"
+  },
+  {
+    "revision": "aa825d8dc04376b09e0ba118d2ff6a71",
+    "url": "/FootPrint/static/media/gallery-1.aa825d8d.jpg"
+  },
+  {
+    "revision": "0e39a342073289150af01449ca0ae627",
+    "url": "/FootPrint/static/media/gallery-2.0e39a342.jpg"
+  },
+  {
+    "revision": "530cee6acf88fbd249a718e2218ce257",
+    "url": "/FootPrint/static/media/gallery-3.530cee6a.jpg"
+  },
+  {
+    "revision": "17d189bb3889d3f6bd5c701810cbb029",
+    "url": "/FootPrint/static/media/jevin.17d189bb.jpg"
+  },
+  {
+    "revision": "5b1ff673fda5957a6f38c854166c1600",
+    "url": "/FootPrint/static/media/kent.5b1ff673.jpg"
+  },
+  {
+    "revision": "0c98b3166410077a65f7c04db9c426f5",
+    "url": "/FootPrint/static/media/kirb.0c98b316.jpg"
+  },
+  {
+    "revision": "68eb086a25e9984f835758d51b52ca85",
+    "url": "/FootPrint/static/media/lui.68eb086a.jpg"
+  },
+  {
+    "revision": "da956eead06cd03ebdb6955a7552ad7b",
+    "url": "/FootPrint/static/media/nat-1.da956eea.jpg"
+  },
+  {
+    "revision": "d30218fed549377971026d9071010e62",
+    "url": "/FootPrint/static/media/nat-2.d30218fe.jpg"
+  },
+  {
+    "revision": "0c5d1bda47915a415be1ee5f20711abd",
+    "url": "/FootPrint/static/media/nat-3.0c5d1bda.jpg"
+  },
+  {
+    "revision": "f60b2eb0bd3778f4345295997e0f730f",
+    "url": "/FootPrint/static/media/sea.f60b2eb0.jpg"
+  },
+  {
+    "revision": "904270101982680eaa40faa5d8025083",
+    "url": "/FootPrint/static/media/sprite.90427010.svg"
+  }
+]);
